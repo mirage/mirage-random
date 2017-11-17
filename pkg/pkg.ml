c437d889353efbd3ecc7404416fa91eb0d3c7ee7
Hannes Mehnert <hannes@mehnert.org>
2017-11-17 14:07:40 +0000

    test 4.04.2 and 4.06, cleanup pkg subdir

--- pkg/pkg.ml
+++ pkg/pkg.ml
@@ -1,11 +1,3 @@
 #!/usr/bin/env ocaml
 #use "topfind"
-#require "topkg"
-open Topkg
-
-let () =
-  Pkg.describe "mirage-random" @@ fun c ->
-  Ok [ Pkg.lib "pkg/META";
-       Pkg.lib ~exts:Exts.interface "src/mirage_random" ;
-       Pkg.mllib "std/stdlibrandom.mllib"
-     ]
+#require "topkg-jbuilder.auto"
